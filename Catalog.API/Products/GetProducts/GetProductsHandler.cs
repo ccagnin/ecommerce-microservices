f86a18608c26b3b41a104540eab16850e7c15d0a
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 18:30:46 -0300

    get products handler comments

--- Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,15 +1,15 @@
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery() : IQuery<GetProductsResult>; // Query object
 
-public record GetProductsResult(IEnumerable<Product> Products);
+public record GetProductsResult(IEnumerable<Product> Products); // Result object
 
-internal class GetProductsQueryHandler
+internal class GetProductsQueryHandler // Query handler
     (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
-    : IQueryHandler<GetProductsQuery, GetProductsResult>
+    : IQueryHandler<GetProductsQuery, GetProductsResult> // Handler interface
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
-    {
+    { // Handler implementation
         logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
 
         var products = await session.Query<Product>().ToListAsync(cancellationToken);
