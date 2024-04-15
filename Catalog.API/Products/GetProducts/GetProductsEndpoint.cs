48802d4fe979634c41cebc8198107ae48bc6a40e
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 18:38:52 -0300

    create get producs endpoint

--- Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,22 @@
 namespace Catalog.API.Products.GetProducts;
 
-public class GetProductsEndpoint
+public record GetProductsResponse(IEnumerable<Product> Products); // Response object
+public class GetProductsEndpoint : ICarterModule 
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery()); // Send query to handler
+
+                var response = result.Adapt<GetProductsResponse>(); // Map result to response
+
+                return Results.Ok(response); // Return response
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products"); // Add metadata
+    }
 }
