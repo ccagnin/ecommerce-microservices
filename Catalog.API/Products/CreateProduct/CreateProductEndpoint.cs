95bd3f05e3e42692e9268f43099fbfd3c39e0c70
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 14:16:34 -0300

    install carter to add routes

--- Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,20 @@
+using Carter;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndpoint
+public record CreateProductRequest(
+    string Name, 
+    List<string> Category, 
+    string Description, 
+    string ImageFile, 
+    decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        
+    }
 }
