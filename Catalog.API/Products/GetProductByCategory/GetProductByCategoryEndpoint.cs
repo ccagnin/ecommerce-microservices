b6baa0b37d87c9d02dc5022b96084448a731be32
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 20:00:01 -0300

    add get product by category endpoint

--- Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -1,6 +1,22 @@
 namespace Catalog.API.Products.GetProductByCategory;
 
-public class GetProductByCategoryEndpoint
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("products/category/{category}", async (ISender sender, string category) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+                var response = result.Adapt<GetProductByCategoryResponse>();
+
+                return Results.Ok(response);
+            })
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products by Category")
+            .WithDescription("Get Products by Category");
+    }
 }
