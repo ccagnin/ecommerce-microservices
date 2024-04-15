3d71508e23baaef9a2219a9d8ed346879b9b1a5b
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 20:27:02 -0300

    add update product handler and endpoint

--- Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -14,6 +14,7 @@ public class GetProductByCategoryEndpoint : ICarterModule
 
                 return Results.Ok(response);
             })
+            .WithName("GetProductByCategory")
             .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Get Products by Category")
