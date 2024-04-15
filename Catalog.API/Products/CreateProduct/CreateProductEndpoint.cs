ab4e9e59f732fbf4dd6223cfb472ce5fbafe8912
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-15 14:25:07 -0300

    add mapster and create post products endpoint

--- Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,6 @@
 using Carter;
+using Mapster;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -15,6 +17,20 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
