e13e7cfa67f8926789020007abca7bb49f0e72be
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-23 20:57:19 -0300

    add: validation with fluentvalidator

--- Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(
@@ -8,6 +10,18 @@ public record CreateProductCommand(
     decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
 
 internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
