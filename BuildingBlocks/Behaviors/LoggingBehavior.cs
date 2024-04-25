0f9d72836913e7f309f865d066c930dc3d75ea28
Caroline <cagnin.caroline@gmail.com>
2024-04-25 17:19:47 -0300

    refactor: remove ilogger of crud and fix logging behavior

--- BuildingBlocks/Behaviors/LoggingBehavior.cs
+++ BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -8,7 +8,7 @@ namespace BuildingBlocks.Behaviors;
 public class LoggingBehavior<TRequest, TResponse>
     (ILogger<LoggingBehavior<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull
+    where TRequest : notnull, IRequest<TResponse>
     where TResponse : notnull
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
--- Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -5,12 +5,11 @@ public record GetProductsQuery() : IQuery<GetProductsResult>; // Query object
 public record GetProductsResult(IEnumerable<Product> Products); // Result object
 
 internal class GetProductsQueryHandler // Query handler
-    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    (IDocumentSession session)
     : IQueryHandler<GetProductsQuery, GetProductsResult> // Handler interface
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     { // Handler implementation
-        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
 
         var products = await session.Query<Product>().ToListAsync(cancellationToken);
         return new GetProductsResult(products);
